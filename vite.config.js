35e9604beccbe259c652a510bf5095242b5bdbf0
moyuderen <moyuderen2021@163.com>
2022-01-13 19:15:35 +0800

    feat: json

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vueJsx from "@vitejs/plugin-vue-jsx"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'json-editor',
   plugins: [vue(), vueJsx()],
   alias: {
       "@": path.resolve(__dirname, "src"),
